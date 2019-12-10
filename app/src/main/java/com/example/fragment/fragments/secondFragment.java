defebf030432914a1028561f7ba49c274504fe88
parashghale <parash.ghale20@gmail.com>
2019-12-10 08:52:24 +0545

    area of cicrle

--- app/src/main/java/com/example/fragment/fragments/secondFragment.java
+++ app/src/main/java/com/example/fragment/fragments/secondFragment.java
@@ -8,14 +8,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.fragment.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class secondFragment extends Fragment {
-
+public class secondFragment extends Fragment implements View.OnClickListener{
+    EditText etRadius;
+    Button btnAreaOfCircle;
 
     public secondFragment() {
         // Required empty public constructor
@@ -26,7 +30,19 @@ public class secondFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view= inflater.inflate(R.layout.fragment_second, container, false);
+        etRadius =view.findViewById(R.id.etRadius);
+        btnAreaOfCircle = view.findViewById(R.id.btnAreaOfCircle);
+
+        btnAreaOfCircle.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        float radius =float.parseFloat(etRadius.getText().toString());
+        float area=3.14f * radius * radius;
+
+        Toast.makeText(getActivity(), "area of cicrle", Toast.LENGTH_SHORT).show();
+    }
 }
